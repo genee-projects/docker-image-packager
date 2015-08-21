29b17c2728623216297dddeefc53185d150da73d
stenote <rui.ma@geneegroup.com>
2015-08-21 09:55:09 +0800

    transfer.go rename to packager.go

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 docker run \
 		--rm \
-		-v "$PWD":/go/src/github.com/genee-tools/docker-images-transfer \
-		-w /go/src/github.com/genee-tools/docker-images-transfer \
+		-v "$PWD":/go/src/github.com/genee-tools/docker-images-packager \
+		-w /go/src/github.com/genee-tools/docker-images-packager \
 		golang:latest \
-		bash -c 'cd /go/src/github.com/genee-tools/docker-images-transfer && go build -o transfer'
+		bash -c 'cd /go/src/github.com/genee-tools/docker-images-packager && go build -o packager'
+
+echo "done!"
--- copy/load.sh
+++ copy/load.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
 for i in *.tar; do
+        echo "$i"
 		docker load -i "$i"
 done
+
+echo "done"
